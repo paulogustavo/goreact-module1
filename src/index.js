82ded9ace5a8db330a3b62f7c993ee7567b5ec92
Paulo Gustavo <pauloaguiar@seap.ma.gov.br>
2019-08-18 15:24:41 -0300

    Adicionando plugin do babel

--- src/index.js
+++ src/index.js
@@ -3,18 +3,19 @@ import {render} from 'react-dom';
 import PropTypes from 'prop-types'
 
 class Button extends Component{
+
+    static defaultProps = {
+        children: "Salvar"
+    }
+    
+    static propTypes = {
+        onClick: PropTypes.func.isRequired,
+        children: PropTypes.string
+    }
+
     render(){
         return <button onClick={this.props.onClick}>{this.props.children}</button>
     }
-}
-
-Button.defaultProps = {
-    children: "Salvar"
-}
-
-Button.propTypes = {
-    onClick: PropTypes.func.isRequired,
-    children: PropTypes.string
 }
 
 class App extends Component{
