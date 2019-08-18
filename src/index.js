b04bb1c44deaebd54821e1d1e5f6c70a6fdbfaea
Paulo Gustavo <pauloaguiar@seap.ma.gov.br>
2019-08-18 04:02:10 -0300

    proptypes added

--- src/index.js
+++ src/index.js
@@ -1,18 +1,33 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
+import PropTypes from 'prop-types'
 
 class Button extends Component{
     render(){
-        return <button>Clica aqui</button>
+        return <button onClick={this.props.onClick}>{this.props.children}</button>
     }
 }
 
+Button.defaultProps = {
+    children: "Salvar"
+}
+
+Button.propTypes = {
+    onClick: PropTypes.func.isRequired,
+    children: PropTypes.string
+}
+
 class App extends Component{
+    handleClick(){
+         alert("Botão clicado!");
+    }
+
     render(){
         return (
         <React.Fragment>
             <h1>Olá, mundo - Módulo 1</h1>
-            <Button />
+            <Button onClick={() => alert("Clicou no primeiro botão!")} />
+            <Button onClick={this.handleClick}>Enviar agora</Button>
         </React.Fragment>);
     }
 }
